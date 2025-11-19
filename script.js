328964048979cc4e1f5f953bcfed8f195aeb6a7f
adlerfloterr-coder <adlerfloterr@gmail.com>
2025-11-19 13:51:01 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let slider = document.querySelector('.slider img');
-let imagens = ['images/banner.jpg'];
-let index = 0;
-setInterval(() => {
-    index = (index + 1) % imagens.length;
-    slider.src = imagens[index];
-}, 4000);
